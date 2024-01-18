b6164adac1439641fa38c82794012d16105ac761
OliverTUBAF <rebentro@hrz.tu-freiberg.de>
2024-01-18 08:37:27 +0100

    Update autocommit-git.sh: Added '-p PATH' option to provide repopath from cli

--- autocommit-git.sh
+++ autocommit-git.sh
@@ -1,17 +1,43 @@
-#!/usr/local/bin/bash
-##!/usr/bin/env bash
+#!/usr/bin/env bash
+##!/bin/bash
 MESSAGE="Auto-commit: $(date +%Y-%m-%d_%H-%M)"
+# Staticly provide the path to repo. When "-p" option is given, this will overwritten.
 REPO_PATH="/path/to/git/repo"
 export PATH=$PATH:/usr/local/bin:/root/bin
 #echo $PATH
 
+function show_help {
+	SCRIPT_NAME=$(basename "$0")
+	echo "$SCRIPT_NAME - Automatically commits a local git repository."
+	echo "Usage: $SCRIPT_NAME [options]"
+	echo ""
+	echo "Options:"
+	echo "-p PATH		Path to local repository."
+	echo "-h		Show this help"
+}
+
+#echo "Analyzing given options \"$*\""
+while getopts ":p:h" opt; do
+    	case $opt in
+		p) REPO_PATH="${OPTARG}" ;;
+		h) show_help; exit 1;;
+		\?) echo "Unknown option: -${OPTARG}";;
+		:) echo "Option -$OPTARG requires an argument." >&2; exit 1;;
+	esac
+done
+
+
 # Check if git command is available
-if ! (which -s git); then
+if ! (which git 2>&1 >>/dev/null); then
 	echo "git command not found. Please install git or fix \$PATH variable."
 	exit 1
 fi
 
 # Check if REPO_PATH is a git repository
+if [ ! -d  $REPO_PATH ]; then
+	echo "Path \"${REPO_PATH}\" does not exist."
+	exit 1
+fi
 git -C "$REPO_PATH" status >/dev/null 2>&1 
 if [ ! $? -eq 0 ]; then
 	echo "Path \"${REPO_PATH}\" not initialized as git repository, please execute manually:"
